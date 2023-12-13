c3ff7f674c3f5dae7702eca5c0f0ac7691374c5f
Lucas Sanchez <lucas.sanchez.dev@gmail.com>
2023-12-13 23:32:34 +0100

    feat: add day 12 part 2

--- 2023/day-12.js
+++ 2023/day-12.js
@@ -15,51 +15,127 @@ const lines = input.map((line) => {
   ];
 });
 
-const isSequenceValid = (sequence, groups) => {
-  const splittedSequence = sequence.split('.').filter((s) => s !== '');
-  if (splittedSequence.length !== groups.length) {
-    return false;
-  }
-  for (let i = 0; i < groups.length; i++) {
-    const currentGroup = groups[i];
-    const currentSequence = splittedSequence[i];
-    if (currentGroup !== currentSequence.length) {
-      return false;
+const splitPossibilities = (possibilities, currentIndex, groups) => {
+  const newPossibilities = {};
+
+  for (let j = 0; j < possibilities.length; j++) {
+    let possibility2Valid = true;
+    const possibility = possibilities[j];
+    const newPossibility1 = {
+      ...possibility,
+      sequence:
+        possibility.sequence.substring(0, currentIndex) +
+        '#' +
+        possibility.sequence.substring(currentIndex + 1),
+    };
+    const newPossibility2 = {
+      ...possibility,
+      sequence:
+        possibility.sequence.substring(0, currentIndex) +
+        '.' +
+        possibility.sequence.substring(currentIndex + 1),
+    };
+    if (
+      newPossibility2.groupCharactersAmount > 0 &&
+      newPossibility2.groupCharactersAmount !== groups[newPossibility2.groupIdx]
+    ) {
+      possibility2Valid = false;
+    }
+    if (newPossibility2.groupCharactersAmount > 0) {
+      newPossibility2.groupIdx++;
+      newPossibility2.groupCharactersAmount = 0;
+    }
+    const newPossibilitiesKey1 = `${newPossibility1.groupIdx};${newPossibility1.groupCharactersAmount};${newPossibility1.sequence[currentIndex]}`;
+    const newPossibilitiesKey2 = `${newPossibility2.groupIdx};${newPossibility2.groupCharactersAmount};${newPossibility2.sequence[currentIndex]}`;
+    if (!newPossibilities[newPossibilitiesKey1]) {
+      newPossibilities[newPossibilitiesKey1] = newPossibility1;
+    } else {
+      newPossibilities[newPossibilitiesKey1].permutations +=
+        newPossibility1.permutations;
+    }
+    if (!possibility2Valid) continue;
+    if (!newPossibilities[newPossibilitiesKey2]) {
+      newPossibilities[newPossibilitiesKey2] = newPossibility2;
+    } else {
+      newPossibilities[newPossibilitiesKey2].permutations +=
+        newPossibility2.permutations;
     }
   }
-  return true;
+  return Object.values(newPossibilities);
 };
 
 const getNbPossibilitiesForHiddenSequence = (hiddenSequence, groups) => {
-  let hiddenChars = 0;
-  let possibilities = 0;
-  for (const char of hiddenSequence) {
-    if (char === '?') {
-      hiddenChars++;
-    }
-  }
-  for (let i = 0; i < 2 ** hiddenChars; i++) {
-    const binary = i.toString(2).padStart(hiddenChars, '0');
-    const binaryArray = binary.split('');
-    let newSequence = '';
-    let binaryIndex = 0;
-    for (const char of hiddenSequence) {
-      if (char === '?') {
-        newSequence += binaryArray[binaryIndex] === '1' ? '#' : '.';
-        binaryIndex++;
-      } else {
-        newSequence += char;
+  let possibilities = [
+    {
+      sequence: hiddenSequence,
+      groupIdx: 0,
+      groupCharactersAmount: 0,
+      permutations: 1,
+    },
+  ];
+  let toRemove = [];
+  for (let i = 0; i < hiddenSequence.length; i++) {
+    if (hiddenSequence[i] === '?')
+      possibilities = splitPossibilities(possibilities, i, groups);
+
+    for (let j = 0; j < possibilities.length; j++) {
+      const possibility = possibilities[j];
+      if (possibility.sequence[i] === '#') {
+        possibility.groupCharactersAmount++;
+        if (
+          possibility.groupCharactersAmount > groups[possibility.groupIdx] ||
+          (possibility.groupCharactersAmount === groups[possibility.groupIdx] &&
+            i < hiddenSequence.length - 1 &&
+            hiddenSequence[i + 1] === '#')
+        ) {
+          toRemove.push(j);
+          continue;
+        }
+      }
+      if (possibility.sequence[i] === '.') {
+        if (
+          possibility.groupCharactersAmount > 0 &&
+          possibility.groupCharactersAmount !== groups[possibility.groupIdx]
+        ) {
+          toRemove.push(j);
+          continue;
+        }
+        if (possibility.groupCharactersAmount > 0) possibility.groupIdx++;
+        possibility.groupCharactersAmount = 0;
       }
     }
-    if (isSequenceValid(newSequence, groups)) {
-      possibilities++;
-    }
+
+    possibilities = possibilities.filter((_, idx) => !toRemove.includes(idx));
+    toRemove = [];
   }
-  return possibilities;
+  return possibilities.reduce((sum, possibility) => {
+    if (
+      (possibility.groupIdx === groups.length &&
+        possibility.groupCharactersAmount === 0) ||
+      (possibility.groupCharactersAmount === groups[possibility.groupIdx] &&
+        possibility.groupIdx === groups.length - 1)
+    )
+      return sum + possibility.permutations;
+    return sum;
+  }, 0);
 };
 
 const possibilitiesSum = lines.reduce((sum, [hiddenSequence, groups], idx) => {
-  return sum + getNbPossibilitiesForHiddenSequence(hiddenSequence, groups);
+  // PART 2
+  hiddenSequence = (hiddenSequence + '?').repeat(5);
+  hiddenSequence = hiddenSequence.substring(0, hiddenSequence.length - 1);
+  groups = (groups.join(',') + ',')
+    .repeat(5)
+    .split(',')
+    .map((nb) => parseInt(nb))
+    .filter((nb) => !isNaN(nb));
+
+  const possibilities = getNbPossibilitiesForHiddenSequence(
+    hiddenSequence,
+    groups
+  );
+
+  return sum + possibilities;
 }, 0);
 
 console.log(possibilitiesSum);
