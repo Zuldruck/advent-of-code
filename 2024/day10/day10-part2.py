c848f7bd71785f41fe6a16ff6a26a07ba6aae0bb
Lucas SANCHEZ <lucas.sanchez.dev@gmail.com>
2024-12-11 13:38:02 +0100

    feat: add day10

---
+++ 2024/day10/day10-part2.py
@@ -0,0 +1,36 @@
+f = open("input.txt", "r")
+grid = [list(map(int,line.strip())) for line in f.readlines()]
+
+def out_of_bounds(pos):
+    return pos[0] < 0 or pos[0] >= len(grid) or pos[1] < 0 or pos[1] >= len(grid[0])
+
+directions = {
+    "UP": (-1, 0),
+    "DOWN": (1, 0),
+    "RIGHT": (0, 1),
+    "LEFT": (0, -1),
+}
+
+def get_trailhead_score(pos, previous_pos):
+    if out_of_bounds(pos):
+        return 0
+    cellHeight = grid[pos[0]][pos[1]]
+    if cellHeight == 9:
+        return 1
+    score = 0
+    for direction in directions:
+        nextCellPos = (pos[0] + directions[direction][0], pos[1] + directions[direction][1])
+        if out_of_bounds(nextCellPos) or nextCellPos == previous_pos:
+            continue
+        nextCellHeight = grid[nextCellPos[0]][nextCellPos[1]]
+        if nextCellHeight == cellHeight + 1:
+            score += get_trailhead_score((pos[0] + directions[direction][0], pos[1] + directions[direction][1]), pos)
+    return score
+
+s = 0
+for i in range(len(grid)):
+    for j in range(len(grid[i])):
+        if grid[i][j] == 0:
+            s += get_trailhead_score((i, j), (-1, -1))
+
+print(s)
