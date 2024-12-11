e7913f81907b7bea8ade126c1ce5c902d7dc1009
Lucas SANCHEZ <lucas.sanchez.dev@gmail.com>
2024-12-11 15:46:20 +0100

    style: remove unused code

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -1,7 +1,7 @@
 f = open("input.txt", "r")
 stones = [int(d) for d in f.read().split()]
 
-for _ in range(10):
+for _ in range(25):
     newStones = []
     for i in range(len(stones)):
         lenNumber = len(str(stones[i]))
@@ -13,12 +13,5 @@ for _ in range(10):
         else:
             newStones.append(stones[i] * 2024)
     stones = newStones
-
-stones_count = {}
-for stone in stones:
-    if stone in stones_count:
-        stones_count[stone] += 1
-    else:
-        stones_count[stone] = 1
 
 print(len(stones))
